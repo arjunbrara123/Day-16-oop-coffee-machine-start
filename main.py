694dd258d9603a1e0dd3950471b44fbb5b8439a7
Arjun <arjunbrara@hotmail.co.uk>
2020-12-06 13:13:02 +0000

    updated main

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+usrReq = ""
+cm = CoffeeMaker()
+cm_menu = Menu()
+mm = MoneyMachine()
+
+while usrReq != "No":
+    usrReq = input(f"What would you like? ({cm_menu.get_items()}): ").lower()
+    if usrReq == "no":
+        break
+    elif usrReq == "report":
+        cm.report()
+    else:
+        drink = cm_menu.find_drink(usrReq)
+        if drink != None:
+            if cm.is_resource_sufficient(drink):
+                if mm.make_payment(drink.cost):
+                    cm.make_coffee(drink)
